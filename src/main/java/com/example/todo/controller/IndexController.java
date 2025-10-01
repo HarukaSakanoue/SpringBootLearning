1a926b806b2044edf263c48a24f03a5b9326d3a0
sakanoue.haruka.sm3 <sakanoue.haruka.sm3@jp.nssol.nipponsteel.com>
2025-10-01 16:16:14 +0900

    初回コミット

--- src/main/java/com/example/todo/controller/IndexController.java
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -2,14 +2,19 @@ package com.example.todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/")
-public class IndexController {
 
-    // http:localhost:8080/ -> "Hello, world!"
-    // GET / -> "Hello, world!"
+@Controller
+public class IndexController{
+
+    //http:localhost:8080/ -. "Hello,world!
+    //Get /-> *Hello,Workd!
+    //no usages new*
     @GetMapping
-    public String index() {
-        return "index";
+    @ResponseBody
+    public String index(){
+        return "Hello,World!";
     }
+
 }
