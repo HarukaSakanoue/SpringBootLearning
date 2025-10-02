3b038358c070245e917ea4af6fe5d2c44af02619
sakanoue.haruka.sm3 <sakanoue.haruka.sm3@jp.nssol.nipponsteel.com>
2025-10-02 14:32:40 +0900

    Mapperの作成

--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -2,26 +2,22 @@ package com.example.todo.service.task;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import com.example.todo.repository.task.TaskRepository;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
 
+    private final TaskRepository taskRepository;
+
+
     public List<TaskEntity> find(){
-        var task1 = new TaskEntity(
-            1,
-            "SpringBootを学ぶ",
-            "TODOアプリケーションを作ってみる",
-            TaskStatus.TODO
-        );
-        var task2 = new TaskEntity(
-            2,
-            "SpringSecurityを学ぶ",
-            "ログイン機能を作ってみる",
-            TaskStatus.DOING
-        );
-        return List.of(task1,task2);
+        
+
+        return taskRepository.select();
 
 
 
