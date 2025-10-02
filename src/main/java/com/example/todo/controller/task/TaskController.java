7cc3a8a9061074a7cad6cf66fd94a33acb91d315
sakanoue.haruka.sm3 <sakanoue.haruka.sm3@jp.nssol.nipponsteel.com>
2025-10-02 10:44:46 +0900

    課題一覧機能の実装

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -2,13 +2,31 @@ package com.example.todo.controller.task;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.List;
 
 
 @Controller
 public class TaskController {
 
-    @GetMapping
-    public String List(){
+    @GetMapping("/tasks")
+    public String List(Model model){
+        var task1 = new TaskDTO(
+            1, 
+            "SpringBootを学ぶ", 
+            "TODOアプリケーションを作ってみる", 
+            "TODO"
+        );
+
+        var task2 = new TaskDTO(
+            2, 
+            "SpringSecurityを学ぶ", 
+            "ログイン機能を作ってみる", 
+            "TODO"
+        );
+        var taskList = List.of(task1,task2);
+        
+        model.addAttribute("taskList", taskList);
         return "tasks/list";
     }
 }
