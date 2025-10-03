48a7f5de7e38fa38cbf6028eb22c2330127fccca
sakanoue.haruka.sm3 <sakanoue.haruka.sm3@jp.nssol.nipponsteel.com>
2025-10-03 15:05:19 +0900

    タスク追加機能の実装

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -2,39 +2,58 @@ package com.example.todo.controller.task;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.todo.service.task.TaskService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/tasks")
 public class TaskController {
     private final TaskService taskService;
 
-
-    @GetMapping("/tasks")
-    public String List(Model model){
+    @GetMapping
+    public String List(Model model) {
         var taskList = taskService.find()
-            .stream()
-            .map(entity -> TaskDTO.toDTO(entity)
-            )
-            .toList();
+                .stream()
+                .map(entity -> TaskDTO.toDTO(entity))
+                .toList();
 
         model.addAttribute("taskList", taskList);
         return "tasks/list";
     }
 
+    @GetMapping("/{id}")
+    public String showDetail(@PathVariable("id") long taskId, Model model) {
 
-@GetMapping("/tasks/{id}")
-    public String showDetail(@PathVariable("id") long taskId, Model model){
-        
         var taskEntity = taskService.findById(taskId)
                 .orElseThrow(() -> new IllegalArgumentException("Task not found : Id =" + taskId));
         model.addAttribute("task", TaskDTO.toDTO(taskEntity));
         return "tasks/detail";
+    }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute TaskForm form) {
+
+        return "tasks/form";
+    }
+
+    @PostMapping
+    public String createTask(@Validated TaskForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return showCreationForm(form);
+        }
+        taskService.create(form.toEntity());
+
+        return "redirect:/tasks";
     }
 }
--- src/main/java/com/example/todo/repository/task/TaskRepository.java
+++ src/main/java/com/example/todo/repository/task/TaskRepository.java
@@ -2,6 +2,7 @@ package com.example.todo.repository.task;
 
 import com.example.todo.service.task.TaskEntity;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Param;
@@ -18,5 +19,8 @@ public interface TaskRepository {
 
   @Select("SELECT id, summary, description, status FROM tasks WHERE id = #{taskId};")
   Optional<TaskEntity> selectById(@Param("taskId") long taskId);
+
+  @Insert("INSERT INTO tasks (summary, description, status) VALUES (#{task.summary}, #{task.description}, #{task.status})")
+  void insert(@Param("task") TaskEntity newEntity);
 
 }
